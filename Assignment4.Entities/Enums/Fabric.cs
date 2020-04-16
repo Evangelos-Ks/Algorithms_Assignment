144cbda5457f8f214e110650f4976d4fa171c07c
Evangelos Koutsogiorgos <www.evangelo@gmail.com>
2020-04-16 10:18:11 +0100

    Add enums and class TShirt

---
+++ Assignment4.Entities/Enums/Fabric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4.Entities.Enums
+{
+    public enum Fabric
+    {
+        Cashmere = 1,
+        Cotton,
+        Linel,
+        Polyester,
+        Rayon,
+        Silk,
+        Wool
+    }
+}
---
+++ Assignment4.Entities/TShirt.cs
@@ -0,0 +1,25 @@
+﻿using Assignment4.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4.Entities
+{
+    class TShirt
+    {
+        //============================= Properties ===========================================================
+        public Color color { get; }
+        public Fabric fabric { get; }
+        public Size size { get; }
+
+        //============================= Constractor ===========================================================
+        public TShirt(Color color, Fabric fabric, Size size)
+        {
+            this.color = color;
+            this.fabric = fabric;
+            this.size = size;
+        }
+    }
+}
