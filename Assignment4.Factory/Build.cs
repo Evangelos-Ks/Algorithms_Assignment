760f830bc9b5bf3a2261bd07234fc9f579cc017b
Evangelos Koutsogiorgos <www.evangelo@gmail.com>
2020-04-17 19:29:07 +0100

    Delete class GenerateData and add class Build

--- Assignment4.Factory/Build.cs
+++ Assignment4.Factory/Build.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Assignment4.Database;
+using Assignment4.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,23 @@ namespace Assignment4.Factory
 {
     class Build
     {
+        public void GenerateAllCombinationsOfShirts()
+        {
+            MyDatabase db = new MyDatabase();
 
+            for (int i = 0; i < db.colors.Count; i++)
+            {
+                for (int j = 0; j < db.fabrics.Count; j++)
+                {
+                    for (int k = 0; k < db.sizes.Count; k++)
+                    {
+                        TShirt ts = new TShirt(db.colors[i], db.fabrics[j], db.sizes[k]);
+                        db.shirts.Add(ts);
+                    }
+                }
+            }
+        }
+
+        
     }
 }
